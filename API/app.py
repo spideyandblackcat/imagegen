4c63e9b9a91f2905c96198c27afd250a2bd9e2b8
spideyandblackcat <166280044+spideyandblackcat@users.noreply.github.com>
2026-01-19 23:33:38 +0600

    Rename hello_world to home and update response

--- API/app.py
+++ API/app.py
@@ -3,5 +3,5 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Your Python app is working on Vercel!'
+def home():
+    return "Running from app.py!"
